acb4fc7bf9c6e92c52d305ff6eaccb6af668af53
olcia2098 <alkri23@student.sdu.dk>
2025-10-27 14:14:12 +0100

    changes db

--- app/src/main/java/com/example/msd/LoginScreen.kt
+++ app/src/main/java/com/example/msd/LoginScreen.kt
@@ -16,7 +16,37 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
 import com.example.msd.ui.theme.MsdTheme
+
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey val username: String,
+    val passwordHash: String,
+    val userType: String
+)
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Query("SELECT * FROM users WHERE username =:username")
+    suspend fun getUser(username: String): User?
+}
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
+
+fun hashPassword(password: String): String = password.reversed()
 
 @Composable
 fun LoginScreen(onLoginClicked: () -> Unit) {
