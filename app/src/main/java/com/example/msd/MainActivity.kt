fc7ea8ec3aaebeace5a9bd02fe3d4a33a276134c
domjab23 <dojab23@student.sdu.dk~>
2025-10-27 11:41:09 +0100

    Add login and pill logging screen

---
+++ app/src/main/java/com/example/msd/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.msd
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.msd.ui.theme.MsdTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MsdTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen(onLoginClicked = { navController.navigate("pill_logging") })
+        }
+        composable("pill_logging") {
+            PillLoggingScreen()
+        }
+    }
+}
