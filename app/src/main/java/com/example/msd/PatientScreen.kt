47c9430d5b8132b88c2e9410cc5085aae0760d48
RadekChy25 <chylak.radoslaw@gmail.com>
2025-11-03 08:59:04 +0100

    Log off works in patient side

--- app/src/main/java/com/example/msd/PatientScreen.kt
+++ app/src/main/java/com/example/msd/PatientScreen.kt
@@ -4,19 +4,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -54,11 +48,12 @@ fun PatientScreen(
         ) {
             composable("home") {
                 PatientHomeScreen(({}),
-                    pills = PillRepository.getPills() ?: emptyList())
+                    pills = PillRepository.getPills()
+                )
             }
             composable("pills") {
                 ViewPillListScreen(
-                    pills = PillRepository.getPills() ?: emptyList(),
+                    pills = PillRepository.getPills(),
                     onPillClicked = ({})
                 )
             }
